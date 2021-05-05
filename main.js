27cc314f5752b3d977d6309b9b1710c0bb3caf7b
Sarah Kendric <kends955@newschool.edu>
2021-05-04 21:48:23 -0400

    update 5/4

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //step 1: set up arrays of oppurtunities 
-var images = ["imgs/1.jpg", "imgs/2.png", "imgs/3.jpg", "imgs/4.png", "imgs/5.jpg"]
-var texts = ["fruits ripen slowly", "<3", "...", "hahaha"]
+var images = ["imgs/1.png", "imgs/2.png", "imgs/3.jpg", "imgs/4.png", "imgs/5.jpg", "imgs/6.jpg", "imgs/7.png", "imgs/8.jpeg"]
+var texts = ["some images from this semester"]
 
 
 //step 2: target existing html
